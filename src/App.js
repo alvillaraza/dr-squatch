0ee736488d377aea326f7fcb3035abd1a19170b5
Alexis <alexis.borja@gmail.com>
2023-04-14 15:50:40 -0700

    modified arr

--- src/App.js
+++ src/App.js
@@ -15,10 +15,32 @@ function App() {
       });
   }, []);
 
+  const prod = products?.map((p) => {
+    if (p.title === 'Clean & Fresh') {
+      return { ...p, scents: ['woodsy', 'fresh'], originalPrice: 4300 };
+    } else if (p.title === 'Top Squatch') {
+      return { ...p, scents: ['woodsy', 'citrus'], originalPrice: 6900 };
+    } else if (p.title === 'Shower Essentials') {
+      return {
+        ...p,
+        scents: ['woodsy', 'citrus', 'herbal'],
+        originalPrice: 4600,
+      };
+    } else if (p.title === 'Deo & Soap Set') {
+      return {
+        ...p,
+        scents: ['woodsy', 'citrus', 'rich', 'spiced', 'herbal'],
+        originalPrice: null,
+      };
+    }
+    return {...p};
+  });
+
+console.log(prod)
   return (
-    <div className='App' products={products}>
+    <div className='App'>
       Dr Squatch
-      <Products products={products} />
+      <Products products={prod} />
     </div>
   );
 }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,4 @@
 const Products = ({ products }) => {
-  // console.log(products);
-
   const priceFormatter = (num) => {
     const price = (num / 100).toFixed(2);
     if (price.endsWith('.00')) {
@@ -9,10 +7,6 @@ const Products = ({ products }) => {
       return price;
     }
   };
-
-  //map through the products,
-  // create a new array
-  // place each item in the newArr, but if the item already exists in that array, add a "x 1", and keep adding to that number?
 
   function includeDups(includedProducts) {
     let sentenceStructure = '';
@@ -45,49 +39,18 @@ const Products = ({ products }) => {
     <section className='products-section'>
       <div>Products</div>
       <div className='products-wrapper'>
-        {/* TODO: We still have to display the original price!! */}
-        {/* TODO: We still have to display the original price!! */}
-        {/* TODO: We still have to display the original price!! */}
         {products?.map((p) => {
-          const scentProfile =
-            p.title === 'Clean & Fresh' ? (
-              <div className='scents'>
-                <h3 className='woodsy'>Woodsy</h3>
-                <h3 className='fresh'>Fresh</h3>
-              </div>
-            ) : p.title === 'Top Squatch' ? (
-              <div className='scents'>
-                <h3 className='woodsy'>Woodsy</h3>
-                <h3 className='citrus'>Citrus</h3>
-              </div>
-            ) : p.title === 'Shower Essentials' ? (
-              <div className='scents'>
-                <h3 className='woodsy'>Woodsy</h3>
-                <h3 className='citrus'>Citrus</h3>
-                <h3 className='herbal'>Herbal</h3>
-              </div>
-            ) : p.title === 'Deo & Soap Set' ? (
-              <div className='scents'>
-                <h3 className='woodsy'>Woodsy</h3>
-                <h3 className='citrus'>Citrus</h3>
-                <h3 className='rich'>Rich</h3>
-                <h3 className='spiced'>Spiced</h3>
-                <h3 className='herbal'>Herbal</h3>
-              </div>
-            ) : null;
           return (
             <div className='products'>
               <img src={p.imageSrc} alt={p.title} />
               <h2>{p.title}</h2>
               <div className='price-wrapper'>
                 {p.originalPrice && (
-                  <h2 className='price-original'>
-                    some price ${priceFormatter(p.price)}
-                  </h2>
+                  <h2 className='price-original'>${priceFormatter(p.originalPrice)}</h2>
                 )}
                 <h2 className='price'>${priceFormatter(p.price)}</h2>
               </div>
-              <p>{scentProfile}</p>
+              <p>{p.scents}</p>
               <p>Included</p>
               <p className='included'> {includeDups(p.products_included)}</p>
             </div>
