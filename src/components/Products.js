2d1ad61f419fa5ea8e2d53ad87a44bb3bd356853
Alexis <alexis.borja@gmail.com>
2023-04-14 11:10:10 -0700

    styling products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,19 +1,20 @@
-/* eslint-disable no-unreachable */
 const Products = ({ products }) => {
   console.log(products);
   return (
-    <section>
-      <div>Products</div>{' '}
-      {products?.map((p) => {
-        return (
-          <>
-            <img src={p.imageSrc} alt={p.title} />
-            <p>{p.title}</p>
-            <p>{p.price}</p>
-            <p>Included {p.products_included}</p>
-          </>
-        );
-      })}
+    <section className='products-section'>
+        <div>Products</div>
+      <div className='products-wrapper'>
+        {products?.map((p) => {
+          return (
+            <div className='products'>
+              <img src={p.imageSrc} alt={p.title} />
+              <p>{p.title}</p>
+              <p>{p.price}</p>
+              <p>Included {p.products_included}</p>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
