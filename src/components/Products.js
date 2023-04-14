8bef7dc1baa92c47ea11b76c2c68a9814bdf6539
Alexis <alexis.borja@gmail.com>
2023-04-14 15:23:38 -0700

    WIP included

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,5 @@
 const Products = ({ products }) => {
-  console.log(products);
+  // console.log(products);
 
   const priceFormatter = (num) => {
     const price = (num / 100).toFixed(2);
@@ -10,10 +10,48 @@ const Products = ({ products }) => {
     }
   };
 
+  //map through the products,
+  // create a new array
+  // place each item in the newArr, but if the item already exists in that array, add a "x 1", and keep adding to that number?
+
+  function includeDups(includedProducts) {
+    let sentenceStructure = '';
+
+    const counts = includedProducts.reduce(
+      (acc, value) => ({
+        ...acc,
+        [value]: (acc[value] || 0) + 1,
+      }),
+      {},
+    );
+    for (const [key, value] of Object.entries(counts)) {
+      const name = key.split('-');
+
+      const newname = name
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+
+      // const temp = name.split(' ');
+      console.log(newname);
+
+      // const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+      // sentenceStructure += `${nameCapitalized}${
+      //   value > 1 ? ` x ${value}` : ''
+      // }`;
+      // sentenceStructure +=
+      //   Object.keys(counts)[Object.keys(counts).length - 1] === key ? '' : ', ';
+    }
+
+    return sentenceStructure;
+  }
+
   return (
     <section className='products-section'>
       <div>Products</div>
       <div className='products-wrapper'>
+        {/* TODO: We still have to display the original price!! */}
+        {/* TODO: We still have to display the original price!! */}
+        {/* TODO: We still have to display the original price!! */}
         {products?.map((p) => {
           const scentProfile =
             p.title === 'Clean & Fresh' ? (
@@ -45,10 +83,17 @@ const Products = ({ products }) => {
             <div className='products'>
               <img src={p.imageSrc} alt={p.title} />
               <h2>{p.title}</h2>
-              <h2 className='price'>${priceFormatter(p.price)}</h2>
+              <div className='price-wrapper'>
+                {p.originalPrice && (
+                  <h2 className='price-original'>
+                    some price ${priceFormatter(p.price)}
+                  </h2>
+                )}
+                <h2 className='price'>${priceFormatter(p.price)}</h2>
+              </div>
               <p>{scentProfile}</p>
-              <h3>Included</h3>
-              <p> {p.products_included}</p>
+              <p>Included</p>
+              <p className='included'> {includeDups(p.products_included)}</p>
             </div>
           );
         })}
